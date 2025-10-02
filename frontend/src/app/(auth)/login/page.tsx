b139c33620e0ebac43d97f689809e32b4bd0273e
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-10-02 15:07:35 +0700

    feat: add background for Login page

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -3,9 +3,26 @@ import { LoginForm } from '@/components/login-form';
 export default function Page() {
     return (
         <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-gray-200">
-            <div className="w-full max-w-md">
-              <LoginForm />
+            <div className=" hidden md:block absolute left-8 top-1/4">
+                <img
+                    src="/images/image_7.png"
+                    alt=""
+                    className="w-48 h-48 lg:w-64 lg:h-64 object-contain"
+                />
+            </div>
+
+            <div className="w-full max-w-sm">
+                <LoginForm />
+            </div>
+
+            <div className="hidden md:block absolute right-8 bottom-1/4">
+                <img
+                    src="/images/image_6.png"
+                    alt=""
+                    className="w-48 h-48 lg:w-64 lg:h-64 object-contain"
+                />
             </div>
         </div>
     );
 }
+
