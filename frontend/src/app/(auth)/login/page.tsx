790deb9d06dc4b3235db7a1abbb252c1fd6e1916
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-10-02 16:36:16 +0700

    fix: change Image nextjs

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,12 +1,14 @@
 import { LoginForm } from '@/components/login-form';
-
+import Image from 'next/image';
 export default function Page() {
     return (
         <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-gray-200">
-            <div className=" hidden md:block absolute left-8 top-1/4">
-                <img
+            <div className=" hidden md:block absolute left-26 top-1/5">
+                <Image
                     src="/images/image_7.png"
                     alt=""
+                    width={192}
+                    height={192}
                     className="w-48 h-48 lg:w-64 lg:h-64 object-contain"
                 />
             </div>
@@ -15,10 +17,12 @@ export default function Page() {
                 <LoginForm />
             </div>
 
-            <div className="hidden md:block absolute right-8 bottom-1/4">
-                <img
+            <div className="hidden md:block absolute right-26 bottom-1/5">
+                <Image
                     src="/images/image_6.png"
                     alt=""
+                    width={192}
+                    height={192}
                     className="w-48 h-48 lg:w-64 lg:h-64 object-contain"
                 />
             </div>
