5348f3aa6aba8fca3e01e00bd93ce359726e634a
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-10-02 15:11:39 +0700

    fix: format check prettier

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
 
@@ -9,7 +9,9 @@ export default function Home() {
         <div className="flex min-h-svh flex-col items-center justify-center">
             <Button>Click me</Button>
             <br />
-            <Button onClick={() => router.push('/login')}>Click me login</Button>
+            <Button onClick={() => router.push('/login')}>
+                Click me login
+            </Button>
         </div>
     );
 }
