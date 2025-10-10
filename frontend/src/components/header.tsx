56457c4e1c2341b497135bcaf1f874b9e6296624
nmChaau <chaunmse180582@fpt.edu.vn>
2025-10-10 12:35:53 +0700

    feat(header): add  user menu and search input field

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,13 +1,20 @@
 'use client';
-import { UserPlus } from 'lucide-react';
+import { Bell, CirclePlus, Search, UserPlus } from 'lucide-react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import React from 'react';
+import { Input } from './ui/input';
+import { UserMenu } from './user-menu';
 
-export default function Header() {
+type HeaderProps = {
+    isLogged: boolean;
+    showInput: boolean;
+};
+
+export default function Header({ isLogged, showInput }: HeaderProps) {
     const router = useRouter();
     return (
-        <header className="flex items-center justify-between px-6 py-5 bg-white shadow-md shadow-gray-200">
+        <header className="flex items-center justify-between px-4 py-3 bg-white shadow-md shadow-gray-200">
             {/* Logo */}
             <div
                 onClick={() => {
@@ -18,27 +25,53 @@ export default function Header() {
                 <Image src="/logo.png" alt="Logo" width={48} height={48} />
                 <span className="font-bold text-2xl">AKA</span>
             </div>
-            <div></div>
+            {showInput && (
+                <div>
+                    <div className="flex w-xl">
+                        <div className="relative w-full pr-5 ">
+                            <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
+                            <Input
+                                type="text"
+                                placeholder="Search..."
+                                className="pl-12 bg-gray-100 border-0 w-full rounded-full text-2xl"
+                            />
+                        </div>
+                    </div>
+                </div>
+            )}
             {/* Menu */}
-            <ul className="flex items-center gap-5">
-                <button
-                    onClick={() => {
-                        router.push('/register');
-                    }}
-                    className="flex items-center bg-black text-white py-3 px-6 rounded-md gap-2 font-bold "
-                >
-                    <UserPlus size={18} />
-                    Register
-                </button>
-                <button
-                    onClick={() => {
-                        router.push('/login');
-                    }}
-                    className="bg-gray-200 text-black py-3 px-6 rounded-md font-bold"
-                >
-                    Login
-                </button>
-            </ul>
+            {isLogged === false ? (
+                <ul className="flex items-center gap-5">
+                    <button
+                        onClick={() => {
+                            router.push('/register');
+                        }}
+                        className="flex items-center bg-black text-white py-3 px-6 rounded-md gap-2 font-bold "
+                    >
+                        <UserPlus size={18} />
+                        Register
+                    </button>
+                    <button
+                        onClick={() => {
+                            router.push('/login');
+                        }}
+                        className="bg-gray-200 text-black py-3 px-6 rounded-md font-bold"
+                    >
+                        Login
+                    </button>
+                </ul>
+            ) : (
+                <div className="flex items-center gap-7">
+                    <button className="rounded-full bg-gray-300 w-25 h-9 flex items-center gap-1 justify-center ">
+                        <CirclePlus size={15} />
+                        Create
+                    </button>
+
+                    <Bell size={30} className="text-gray-400" />
+
+                    <UserMenu />
+                </div>
+            )}
         </header>
     );
 }
