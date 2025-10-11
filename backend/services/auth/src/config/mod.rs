15063ffb7ad08b3fc6ca242bcf9ba7ced67c5c76
akagiyuu <khanghuynh0245@gmail.com>
2025-10-11 12:45:19 +0700

    feat(auth): add config

---
+++ backend/services/auth/src/config/mod.rs
@@ -0,0 +1,28 @@
+use std::sync::LazyLock;
+
+use serde::Deserialize;
+
+const fn default_port() -> u16 {
+    3000
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    #[serde(default = "default_port")]
+    pub port: u16,
+
+    pub database_url: String,
+}
+
+pub static CONFIG: LazyLock<Config> = LazyLock::new(|| {
+    ::config::Config::builder()
+        .add_source(
+            ::config::Environment::default()
+                .try_parsing(true)
+                .separator("__"),
+        )
+        .build()
+        .unwrap()
+        .try_deserialize()
+        .unwrap()
+});
