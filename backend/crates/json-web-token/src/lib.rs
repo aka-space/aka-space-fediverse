d6c1455a240980b4efc027cfe0a660fcb0e07370
akagiyuu <khanghuynh0245@gmail.com>
2025-10-13 16:54:05 +0700

    feat(jwt): add jwt creation function

--- backend/crates/json-web-token/src/lib.rs
+++ backend/crates/json-web-token/src/lib.rs
@@ -1,5 +1,25 @@
 mod claims;
 mod config;
 
+use jsonwebtoken::{DecodingKey, EncodingKey};
+
 pub use claims::Claims;
 pub use config::Config;
+
+pub struct Jwt {
+    pub encoding_key: EncodingKey,
+    pub decoding_key: DecodingKey,
+    pub expired_in: u64,
+}
+
+impl Jwt {
+    pub fn new() -> Result<Self, ::config::ConfigError> {
+        let cfg = Config::new()?;
+
+        Ok(Self {
+            encoding_key: EncodingKey::from_secret(cfg.secret.as_bytes()),
+            decoding_key: DecodingKey::from_secret(cfg.secret.as_bytes()),
+            expired_in: cfg.expired_in,
+        })
+    }
+}
