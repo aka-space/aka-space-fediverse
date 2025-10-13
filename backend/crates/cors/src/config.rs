5475ea0e9b7ef4900b776757f2f54c7279c2bfd9
akagiyuu <khanghuynh0245@gmail.com>
2025-10-13 16:38:29 +0700

    refactor(cors): split config into multiple files

--- backend/crates/cors/src/config.rs
+++ backend/crates/cors/src/config.rs
@@ -1,31 +1,6 @@
-use axum::http::{
-    HeaderName, HeaderValue, Method,
-    header::{
-        ACCEPT, ACCESS_CONTROL_ALLOW_HEADERS, ACCESS_CONTROL_ALLOW_METHODS,
-        ACCESS_CONTROL_ALLOW_ORIGIN, AUTHORIZATION, CONTENT_TYPE, InvalidHeaderValue, ORIGIN,
-    },
-};
 use serde::Deserialize;
-use tower_http::cors::CorsLayer;
 
 const ENV_PREFIX: &str = "CORS";
-
-const ALLOW_HEADERS: [HeaderName; 7] = [
-    ORIGIN,
-    AUTHORIZATION,
-    ACCESS_CONTROL_ALLOW_ORIGIN,
-    CONTENT_TYPE,
-    ACCEPT,
-    ACCESS_CONTROL_ALLOW_METHODS,
-    ACCESS_CONTROL_ALLOW_HEADERS,
-];
-const ALLOW_METHODS: [Method; 5] = [
-    Method::GET,
-    Method::POST,
-    Method::DELETE,
-    Method::PATCH,
-    Method::PUT,
-];
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
@@ -33,7 +8,7 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn from_env() -> Result<Config, config::ConfigError> {
+    pub fn new() -> Result<Config, config::ConfigError> {
         config::Config::builder()
             .add_source(
                 config::Environment::default()
@@ -43,22 +18,5 @@ impl Config {
             )
             .build()
             .and_then(|x| x.try_deserialize())
-    }
-
-    pub fn build(self) -> Result<CorsLayer, InvalidHeaderValue> {
-        let origin = self
-            .origin
-            .into_iter()
-            .map(|x| x.parse::<HeaderValue>())
-            .collect::<Result<Vec<_>, _>>()?;
-
-        let layer = CorsLayer::new()
-            .allow_origin(origin)
-            .allow_headers(ALLOW_HEADERS)
-            .expose_headers(ALLOW_HEADERS)
-            .allow_credentials(true)
-            .allow_methods(ALLOW_METHODS);
-
-        Ok(layer)
     }
 }
--- backend/crates/cors/src/lib.rs
+++ backend/crates/cors/src/lib.rs
@@ -1,1 +1,26 @@
-pub mod config;
+mod config;
+mod header;
+mod method;
+
+use axum::http::{HeaderValue, header::InvalidHeaderValue};
+use tower_http::cors::CorsLayer;
+
+pub use crate::config::Config;
+use crate::{header::ALLOW_HEADERS, method::ALLOW_METHODS};
+
+pub fn build(config: Config) -> Result<CorsLayer, InvalidHeaderValue> {
+    let origin = config
+        .origin
+        .into_iter()
+        .map(|x| x.parse::<HeaderValue>())
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let layer = CorsLayer::new()
+        .allow_origin(origin)
+        .allow_headers(ALLOW_HEADERS)
+        .expose_headers(ALLOW_HEADERS)
+        .allow_credentials(true)
+        .allow_methods(ALLOW_METHODS);
+
+    Ok(layer)
+}
