4a119f573f801d3fa669d7e799a70f6c230dcc5c
akagiyuu <khanghuynh0245@gmail.com>
2025-10-13 17:12:35 +0700

    feat(oidc): add providers

---
+++ backend/crates/oidc/src/provider.rs
@@ -0,0 +1,9 @@
+use serde::Deserialize;
+use utoipa::ToSchema;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize, ToSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum Provider {
+    Google,
+    Microsoft,
+}
