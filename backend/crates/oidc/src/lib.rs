8be6a477759f3e2df441019b1c041aa1747fc476
akagiyuu <khanghuynh0245@gmail.com>
2025-10-16 09:38:07 +0700

    feat(oidc): create client

--- backend/crates/oidc/src/lib.rs
+++ backend/crates/oidc/src/lib.rs
@@ -1,5 +1,21 @@
 mod config;
 mod provider;
 
+use openidconnect::{EndpointMaybeSet, EndpointNotSet, EndpointSet, core::CoreClient, reqwest};
+
 pub use config::*;
 pub use provider::Provider;
+
+type InnerClient = CoreClient<
+    EndpointSet,
+    EndpointNotSet,
+    EndpointNotSet,
+    EndpointNotSet,
+    EndpointMaybeSet,
+    EndpointMaybeSet,
+>;
+
+pub struct Client {
+    inner: InnerClient,
+    http_client: reqwest::Client,
+}
