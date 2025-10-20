4f9f97c2d9286abd074d0874961c6914531084d1
akagiyuu <khanghuynh0245@gmail.com>
2025-10-20 16:09:59 +0700

    feat(broadcast): send without waiting for confirm

--- backend/crates/broadcast/src/lib.rs
+++ backend/crates/broadcast/src/lib.rs
@@ -1,5 +1,3 @@
-mod error;
 mod sender;
 
-pub use error::*;
 pub use sender::*;
--- backend/crates/broadcast/src/sender.rs
+++ backend/crates/broadcast/src/sender.rs
@@ -1,4 +1,4 @@
-use std::{marker::PhantomData, str::FromStr};
+use std::marker::PhantomData;
 
 use anyhow::Result;
 use lapin::{BasicProperties, Channel, options::BasicPublishOptions};
@@ -14,13 +14,15 @@ impl<E: AsRef<str>, T: Serialize> Sender<E, T> {
     pub async fn send(&self, event: &E, data: &T) -> Result<()> {
         let serialized = serde_json::to_vec(data)?;
 
-        self.channel.basic_publish(
-            &self.exchange,
-            event.as_ref(),
-            BasicPublishOptions::default(),
-            &serialized,
-            BasicProperties::default().with_delivery_mode(2),
-        );
+        self.channel
+            .basic_publish(
+                &self.exchange,
+                event.as_ref(),
+                BasicPublishOptions::default(),
+                &serialized,
+                BasicProperties::default().with_delivery_mode(2),
+            )
+            .await?;
 
         todo!()
     }
