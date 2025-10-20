d9d9b6b3559ac090246440c9c8757820b849d264
akagiyuu <khanghuynh0245@gmail.com>
2025-10-20 16:12:07 +0700

    feat(broadcast): add sender constructor

--- backend/crates/broadcast/src/sender.rs
+++ backend/crates/broadcast/src/sender.rs
@@ -1,7 +1,11 @@
 use std::marker::PhantomData;
 
 use anyhow::Result;
-use lapin::{BasicProperties, Channel, options::BasicPublishOptions};
+use lapin::{
+    BasicProperties, Channel, Connection, ExchangeKind,
+    options::{BasicPublishOptions, ExchangeDeclareOptions},
+    types::FieldTable,
+};
 use serde::Serialize;
 
 pub struct Sender<E: AsRef<str>, T: Serialize> {
@@ -11,6 +15,27 @@ pub struct Sender<E: AsRef<str>, T: Serialize> {
 }
 
 impl<E: AsRef<str>, T: Serialize> Sender<E, T> {
+    pub async fn new(exchange: String, connection: &Connection) -> Result<Self> {
+        let channel = connection.create_channel().await?;
+        channel
+            .exchange_declare(
+                &exchange,
+                ExchangeKind::Topic,
+                ExchangeDeclareOptions {
+                    durable: true,
+                    ..Default::default()
+                },
+                FieldTable::default(),
+            )
+            .await?;
+
+        Ok(Self {
+            channel,
+            exchange,
+            _phantom: Default::default(),
+        })
+    }
+
     pub async fn send(&self, event: &E, data: &T) -> Result<()> {
         let serialized = serde_json::to_vec(data)?;
 
