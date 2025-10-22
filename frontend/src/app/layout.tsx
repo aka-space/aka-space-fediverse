199590796baed4b7b22b549885f086ec49a24aa3
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-10-22 14:39:16 +0700

    feat(post): add provider for all pages

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/header';
+import QueryProviders from '@/providers/query';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -28,8 +29,10 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
-                <Header isLogged={true} showInput={true} />
-                {children}
+                <QueryProviders>
+                    <Header isLogged={true} showInput={true} />
+                    {children}
+                </QueryProviders>
             </body>
         </html>
     );
