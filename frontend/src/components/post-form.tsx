e57f6e372bc8ee55f81d8497daaf0ff56b6d8e26
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-10-27 17:36:00 +0700

    fix(post): change to use editor ui component

--- frontend/src/components/post-form.tsx
+++ frontend/src/components/post-form.tsx
@@ -2,8 +2,7 @@ import React, { useRef } from 'react';
 import { Input } from './ui/input';
 import { Button } from './ui/button';
 import { Textarea } from './ui/textarea';
-import { FileUpload } from './ui/file-upload';
-import { BookImage } from 'lucide-react';
+import { MinimalTiptapEditor } from './ui/minimal-tiptap';
 
 interface PostFormProps {
     postData: {
@@ -18,39 +17,8 @@ interface PostFormProps {
 }
 
 const PostForm = ({ postData, handleChange, handleSubmit }: PostFormProps) => {
-    const contentRef = useRef<HTMLTextAreaElement>(null);
-
-    const handleAddImage = (imageUrl: string) => {
-        if (!contentRef.current) return;
-        const textarea = contentRef.current;
-        const start = textarea.selectionStart;
-        const end = textarea.selectionEnd;
-        const before = postData.content.slice(0, start);
-        const after = postData.content.slice(end);
-        const markdown = `![image](${imageUrl})`;
-        const newContent = before + markdown + after;
-
-        handleChange({
-            target: {
-                name: 'content',
-                value: newContent,
-            },
-        } as React.ChangeEvent<HTMLTextAreaElement>);
-
-        setTimeout(() => {
-            textarea.focus();
-            textarea.selectionStart = textarea.selectionEnd =
-                start + markdown.length;
-        }, 0);
-    };
-
-    const handleFileUpload = (files: File[]) => {
-        const uploadedImageUrl = URL.createObjectURL(files[0]);
-        handleAddImage(uploadedImageUrl);
-    };
-
     return (
-        <div className="mmax-w-4xl mx-auto mt-8 p-4 bg-transparent">
+        <div className="min-h-screen flex items-center justify-center p-4 bg-transparent">
             <div className="bg-gray-50 dark:bg-neutral-900 border border-gray-300 dark:border-neutral-700 rounded-lg shadow-sm shadow-gray-200 dark:shadow-neutral-800">
                 <div className="bg-white dark:bg-neutral-800 rounded-md p-6 border border-gray-100 dark:border-neutral-700">
                     <h1 className="text-2xl font-semibold mb-4 text-gray-900 dark:text-gray-100">
@@ -71,6 +39,7 @@ const PostForm = ({ postData, handleChange, handleSubmit }: PostFormProps) => {
                                 value={postData.title}
                                 onChange={handleChange}
                                 className="w-full border border-gray-200 bg-white dark:bg-neutral-800 dark:border-neutral-700 rounded-sm"
+                                placeholder='Enter your Title...'
                                 required
                             />
                         </div>
@@ -87,6 +56,7 @@ const PostForm = ({ postData, handleChange, handleSubmit }: PostFormProps) => {
                                 value={postData.overview}
                                 onChange={handleChange}
                                 className="w-full min-h-[84px] resize-vertical border border-gray-200 bg-white dark:bg-neutral-800 dark:border-neutral-700 rounded-sm"
+                                placeholder="Enter your Overview..."
                             />
                         </div>
                         <div className="mb-2">
@@ -96,23 +66,24 @@ const PostForm = ({ postData, handleChange, handleSubmit }: PostFormProps) => {
                             >
                                 Content
                             </label>
-                            <Textarea
-                                ref={contentRef}
-                                id="content"
-                                name="content"
+                            <MinimalTiptapEditor
                                 value={postData.content}
-                                onChange={handleChange}
-                                className="w-full min-h-[220px] resize-vertical border border-gray-200 bg-white dark:bg-neutral-800 dark:border-neutral-700 rounded-sm"
+                                onChange={(value) =>
+                                    handleChange({
+                                        target: { name: 'content', value },
+                                    } as unknown as React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>)
+                                }
+                                className="w-full min-h-60"
+                                editorContentClassName="p-5"
+                                output="html"
+                                placeholder="Enter your Content..."
+                                autofocus={true}
+                                editable={true}
+                                editorClassName="focus:outline-hidden"
                             />
                         </div>
-                        <div className="flex items-center gap-6 mt-6 mb-10">
-                            <div className="flex-3 ml-10">
-                                <FileUpload onChange={handleFileUpload} />
-                            </div>
-
-                            <div className="flex-1" />
-
-                            <div className="flex-1">
+                        <div className="flex items-center justify-end gap-6 mt-6 mb-10">
+                            <div>
                                 <Button
                                     variant="rounded"
                                     size="lg"
@@ -123,7 +94,7 @@ const PostForm = ({ postData, handleChange, handleSubmit }: PostFormProps) => {
                                 </Button>
                             </div>
 
-                            <div className="flex-1">
+                            <div>
                                 <Button
                                     type="submit"
                                     className="px-6 rounded-2xl"
