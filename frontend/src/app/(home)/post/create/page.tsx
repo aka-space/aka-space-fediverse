673f2dfa2e7928a50eae422d5442029dff222160
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-10-27 17:36:54 +0700

    style(post): change style to make the form be between the height and weight of screen

--- frontend/src/app/(home)/post/create/page.tsx
+++ frontend/src/app/(home)/post/create/page.tsx
@@ -31,7 +31,7 @@ const CreatePostForm = () => {
         console.log('Post Data:', postData);
     };
     return (
-        <div className="w-full px-6 mt-10">
+        <div className="w-full px-6">
             <div className="max-w-4xl mx-auto w-full">
                 <PostForm
                     postData={postData}
