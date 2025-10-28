0ec1d4cb873a45a1e5a930a49084747d7180cea9
nmChaau <chaunmse180582@fpt.edu.vn>
2025-10-28 22:23:27 +0700

    chore(schema): add auth schema file

---
+++ frontend/src/schemas/auth-schema.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+
+export const registerSchema = z
+    .object({
+        username: z.string().min(2, 'Username must be at least 2 characters'),
+        email: z.email('Invalid email'),
+        password: z.string().min(8, 'Password must be at least 8 characters'),
+        confirmPassword: z.string().min(8, 'Confirm password is required'),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        message: "Passwords don't match",
+        path: ['confirmPassword'],
+    });
+
+export const loginSchema = z.object({
+    email: z.email('Invalid email'),
+    password: z.string().min(8, 'Password must be at least 8 characters'),
+});
+
+export type RegisterFormData = z.infer<typeof registerSchema>;
+export type LoginFormData = z.infer<typeof loginSchema>;
