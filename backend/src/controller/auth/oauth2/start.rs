deb3f27dcdbfb76abffe182f18bab582fafde886
akagiyuu <khanghuynh0245@gmail.com>
2025-10-29 11:01:55 +0700

    feat(auth): use redis service

--- backend/src/controller/auth/oauth2/start.rs
+++ backend/src/controller/auth/oauth2/start.rs
@@ -8,11 +8,9 @@ use axum_extra::extract::{
     CookieJar,
     cookie::{Cookie, SameSite},
 };
-use redis::AsyncTypedCommands;
-use uuid::Uuid;
 
 use crate::{
-    config::{OAUTH2_TEMPORARY, Provider},
+    config::{OAUTH2_TEMPORARY, Provider, REDIS_SESSION_PREFIX},
     error::{Error, Result},
     state::ApiState,
 };
@@ -32,21 +30,12 @@ pub async fn start(
 ) -> Result<(CookieJar, Redirect)> {
     let (auth_url, csrf, nonce) = state.oauth2_services[&provider].start();
 
-    let Ok(stored) = serde_json::to_string(&(csrf, nonce)) else {
-        tracing::error!("Failed to serialize");
+    let redis_key = state
+        .redis_service
+        .set(REDIS_SESSION_PREFIX, &(csrf, nonce))
+        .await?;
 
-        return Err(Error::internal());
-    };
-
-    let mut connection = state.redis_connection.clone();
-    let id = Uuid::new_v4().to_string();
-    if let Err(error) = connection.set(&id, stored).await {
-        tracing::error!(?error, "Failed to store oauth2 csrf and nonce");
-
-        return Err(Error::internal());
-    }
-
-    let mut cookie = Cookie::new(OAUTH2_TEMPORARY, id);
+    let mut cookie = Cookie::new(OAUTH2_TEMPORARY, redis_key);
     cookie.set_http_only(true);
     cookie.set_same_site(Some(SameSite::None));
     cookie.set_secure(true);
