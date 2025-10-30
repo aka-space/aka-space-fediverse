510ce264e312bf1ddd4d400618f4e2cac16a3d6d
nmChaau <chaunmse180582@fpt.edu.vn>
2025-10-31 00:04:40 +0700

    feat(auth): call real authentication API

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -3,15 +3,20 @@ import { UserLogin, UserRegister } from '@/types';
 import { create } from 'zustand';
 
 interface AuthState {
+    accessToken?: string | null;
     authUser: UserRegister | null;
     isLoggingIn: boolean;
     isRegisting: boolean;
     register: (data: UserRegister) => Promise<boolean | undefined>;
     login: (data: UserLogin) => Promise<boolean | undefined>;
-    logout: () => void;
+    loginWithGG: () => Promise<void>;
+    refreshAccessToken: () => Promise<string | null>;
+    logout: () => Promise<void>;
+    getUser: () => Promise<void>;
+    initAuth: () => Promise<void>;
 }
 
-export const useAuthStore = create<AuthState>((set) => ({
+export const useAuthStore = create<AuthState>((set, get) => ({
     authUser: null,
     isLoggingIn: false,
     isRegisting: false,
@@ -19,13 +24,14 @@ export const useAuthStore = create<AuthState>((set) => ({
     register: async (data: UserRegister) => {
         set({ isRegisting: true });
         try {
-            const res = await axiosInstance.post('/authAka', data);
+            const res = await axiosInstance.post('/auth/register', data);
             if (res) {
-                set({ authUser: data });
+                localStorage.setItem('accessToken', res.data);
+                set({ isLoggingIn: true });
                 return true;
             }
         } catch (error) {
-            console.log(error);
+            console.log('error when register: ', error);
             set({ authUser: null });
         } finally {
             set({ isRegisting: false });
@@ -35,28 +41,82 @@ export const useAuthStore = create<AuthState>((set) => ({
     login: async (data: UserLogin) => {
         set({ isLoggingIn: true });
         try {
-            const res = await axiosInstance.get('/authAka');
-            const user = res.data.filter(
-                (a: UserLogin) => a.email.toString() === data.email,
-            );
-
-            if (user[0].password === data.password) {
-                set({ authUser: user[0] });
+            const res = await axiosInstance.post('/auth/login', data);
+            if (res) {
+                localStorage.setItem('accessToken', res.data);
                 return true;
             }
         } catch (error) {
-            console.log(error);
-            set({ authUser: null });
-        } finally {
-            set({ isLoggingIn: false });
+            console.log('error when login: ', error);
+            set({ authUser: null, isLoggingIn: false });
         }
     },
 
-    logout: () => {
+    loginWithGG: async () => {
+        set({ isLoggingIn: true });
         try {
+            await axiosInstance.get('/oauth2/google');
+        } catch (error) {
+            console.log('error when login with google: ', error);
             set({ authUser: null });
+        }
+    },
+
+    refreshAccessToken: async () => {
+        try {
+            const res = await axiosInstance.post<{ accessToken: string }>(
+                '/auth/refresh',
+                {},
+                { withCredentials: true },
+            );
+            const newToken = res.data.accessToken;
+            localStorage.setItem('accessToken', newToken);
+            set({ accessToken: newToken });
+            return newToken;
+        } catch (error) {
+            console.error('Failed to refresh token:', error);
+            set({ authUser: null, accessToken: null });
+            return null;
+        }
+    },
+
+    logout: async () => {
+        try {
+            const token = localStorage.getItem('accessToken');
+            if (token) {
+                await axiosInstance.post('/auth/logout');
+                localStorage.removeItem('accessToken');
+                set({ authUser: null, isLoggingIn: false });
+                console.log('Logout successful. Refresh cookie removed.');
+            }
         } catch (err) {
-            console.log(err);
+            console.log('Error during logout:', err);
+        }
+    },
+
+    getUser: async () => {
+        const token = localStorage.getItem('accessToken');
+        try {
+            const userRes = await axiosInstance.get('/auth/me', {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            set({ authUser: userRes.data });
+        } catch (error) {
+            console.log('Get user error:', error);
+        }
+    },
+
+    initAuth: async () => {
+        const token = localStorage.getItem('accessToken');
+        if (!token) return;
+        try {
+            get().getUser();
+            set({ isLoggingIn: true });
+        } catch (err) {
+            console.error('initAuth error:', err);
+            localStorage.removeItem('accessToken');
         }
     },
 }));
