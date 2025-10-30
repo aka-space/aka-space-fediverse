e6f9cc3d5baeac856ef334498260429f7ff2aa6f
akagiyuu <khanghuynh0245@gmail.com>
2025-10-30 11:51:10 +0700

    feat(database): add slug column to post

---
+++ backend/migrations/20251030042939_add_slugify_function.up.sql
@@ -0,0 +1,30 @@
+--https://gist.github.com/ianks/f8fe7822515290c3a269a616cac6a03d/
+
+CREATE EXTENSION IF NOT EXISTS "unaccent";
+
+CREATE OR REPLACE FUNCTION slugify("value" TEXT)
+RETURNS TEXT AS $$
+    -- removes accents (diacritic signs) from a given string --
+    WITH "unaccented" AS (
+        SELECT unaccent("value") AS "value"
+    ),
+    -- lowercases the string
+    "lowercase" AS (
+        SELECT lower("value") AS "value"
+        FROM "unaccented"
+    ),
+    -- replaces anything that's not a letter, number, hyphen('-'), or underscore('_') with a hyphen('-')
+    "hyphenated" AS (
+        SELECT regexp_replace("value", '[^a-z0-9\\-_]+', '-', 'gi') AS "value"
+        FROM "lowercase"
+    ),
+    -- trims hyphens('-') if they exist on the head or tail of the string
+    "trimmed" AS (
+        SELECT regexp_replace(regexp_replace("value", '\\-+$', ''), '^\\-', '') AS "value"
+        FROM "hyphenated"
+    )
+    SELECT "value" FROM "trimmed";
+$$ LANGUAGE SQL STRICT IMMUTABLE;
+
+ALTER TABLE posts
+    ADD COLUMN slug text GENERATED ALWAYS AS (slugify(title)) STORED;
