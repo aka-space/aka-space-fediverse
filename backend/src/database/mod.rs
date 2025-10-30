ae010562ce149c92a88adf6e8d2841c7f42bce9a
akagiyuu <khanghuynh0245@gmail.com>
2025-10-30 16:17:47 +0700

    feat(database/post): add get* queries

--- backend/src/database/mod.rs
+++ backend/src/database/mod.rs
@@ -1,2 +1,28 @@
+use serde::Deserialize;
+
 pub mod account;
+pub mod post;
+pub mod reaction;
 pub mod tags;
+
+#[derive(Clone, Copy)]
+pub struct Pagination {
+    pub limit: i64,
+    pub offset: i64,
+}
+
+#[derive(Default, Deserialize)]
+pub enum SortDirection {
+    #[default]
+    Ascending,
+    Descending,
+}
+
+#[derive(Deserialize)]
+pub struct Sort<T> {
+    #[serde(default)]
+    column: T,
+
+    #[serde(default)]
+    direction: SortDirection,
+}
---
+++ backend/src/database/post.rs
@@ -0,0 +1,86 @@
+use chrono::{DateTime, Utc};
+use sqlx::PgExecutor;
+use sqlx_conditional_queries::conditional_query_as;
+use uuid::Uuid;
+
+use crate::database::{Pagination, Sort, SortDirection};
+
+#[derive(Debug)]
+pub struct Post {
+    id: Uuid,
+    slug: String,
+
+    author_id: String,
+    title: String,
+    content: String,
+    view: i32,
+
+    created_at: DateTime<Utc>,
+    updated_at: DateTime<Utc>,
+}
+
+#[derive(Default)]
+pub enum SortablePostColumn {
+    #[default]
+    Id,
+    View,
+    CreatedAt,
+}
+
+pub async fn get_all(
+    sort: Sort<SortablePostColumn>,
+    Pagination { limit, offset }: Pagination,
+    executor: impl PgExecutor<'_>,
+) -> sqlx::Result<Vec<Post>> {
+    conditional_query_as!(
+        Post,
+        r#"
+            SELECT
+                id,
+                slug,
+                author_id,
+                title,
+                content,
+                view,
+                created_at,
+                updated_at
+            FROM posts
+            ORDER BY {#column} {#direction}
+            LIMIT {limit}
+            OFFSET {offset}
+        "#,
+        #column = match sort.column {
+            SortablePostColumn::Id => "id",
+            SortablePostColumn::View => "view",
+            SortablePostColumn::CreatedAt => "created_at",
+        },
+        #direction = match sort.direction {
+            SortDirection::Ascending => "ASC",
+            SortDirection::Descending => "DESC",
+        },
+    )
+    .fetch_all(executor)
+    .await
+}
+
+pub async fn get(slug: String, executor: impl PgExecutor<'_>) -> sqlx::Result<Option<Post>> {
+    sqlx::query_as!(
+        Post,
+        r#"
+            SELECT
+                id,
+                slug,
+                author_id,
+                title,
+                content,
+                view,
+                created_at,
+                updated_at
+            FROM posts
+            WHERE slug = $1
+        "#,
+        slug
+    )
+    .fetch_optional(executor)
+    .await
+}
