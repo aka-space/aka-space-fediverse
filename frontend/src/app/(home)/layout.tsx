d90f8fbf8ef7876cf6a03dcaff0517840058319d
nmChaau <chaunmse180582@fpt.edu.vn>
2025-10-31 19:16:18 +0700

    style(layout)

--- frontend/src/app/(home)/layout.tsx
+++ frontend/src/app/(home)/layout.tsx
@@ -4,7 +4,7 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
     return (
         <div className="min-h-screen w-full flex flex-col bg-background overflow-hidden">
-            <main className="w-full flex justify-center py-6">
+            <main className="w-full flex justify-center py-6 mt-16 px-4">
                 <div className="w-full max-w-7xl px-4">{children}</div>
             </main>
         </div>
