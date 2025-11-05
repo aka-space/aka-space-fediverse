e8a84c9288e905ea4f3f7415f84a50a6c10b34ef
nmChaau <chaunmse180582@fpt.edu.vn>
2025-11-05 17:19:45 +0700

    style(ui): edit backdrop loading component

--- frontend/src/components/loading.tsx
+++ frontend/src/components/loading.tsx
@@ -2,12 +2,9 @@ import { Loader2 } from 'lucide-react';
 
 export default function Loading() {
     return (
-        <div className="flex h-screen items-center justify-center bg-gray-50">
+        <div className="fixed inset-0 flex items-center justify-center backdrop-opacity-10">
             <div className="flex flex-col items-center space-y-4">
-                <Loader2 className="h-10 w-10 text-gray-500 animate-spin" />
-                <p className="text-gray-600 font-medium">
-                    Loading, please wait...
-                </p>
+                <Loader2 className="h-10 w-10 text-gray-50 animate-spin" />
             </div>
         </div>
     );
