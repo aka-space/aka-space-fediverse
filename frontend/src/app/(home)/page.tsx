95b19128aeaa463f9f342bc5f00039eb1ec1db77
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-11-06 11:04:47 +0700

    fix(post): fix the home page

--- frontend/src/app/(home)/page.tsx
+++ frontend/src/app/(home)/page.tsx
@@ -4,34 +4,39 @@ import { PostsFilter } from '@/components/posts-filter';
 import { PostCard } from '@/components/post-card';
 import { Spinner } from '@/components/ui/spinner';
 import { useGetPosts } from '@/hooks/use-get-posts';
-
-type Post = {
-    id: number;
-    author: {
-        name: string;
-        avatar: string;
-    };
-    title: string;
-    content: string;
-    createdAt: string;
-    likes: number;
-    comments: number;
-    shares: number;
-    overview: string;
-    tags: string[];
-};
-const api = 'https://68765855814c0dfa653bba48.mockapi.io/mockTest';
+import { Post } from '@/types';
+import { NoPost } from '@/components/no-post';
+import { useMemo, useState } from 'react';
+import { formatTime } from '@/lib/formatDate';
 
 export default function Home() {
-    const { data: posts, isPending: loading } = useGetPosts(api);
+    const { data: posts, isPending: loading } = useGetPosts();
+    const [filter, setFilter] = useState<string>('new');
+    const sortedPosts = useMemo(() => {
+        if (!posts) return [];
+
+        const postsCopy = [...posts];
+
+        if (filter === 'new') {
+            return postsCopy.sort((a: Post, b: Post) => 
+                formatTime(a.createdAt) - formatTime(b.createdAt)
+            );
+        } else if (filter === 'hot') {
+            return postsCopy.sort((a: Post, b: Post) => b.likes - a.likes);
+        }
+
+        return postsCopy;
+    }, [posts, filter]);
 
     return (
         <div className="w-full flex justify-center mb-6 px-4">
             <div className="flex flex-col gap-6 max-w-4xl w-full">
                 <div className="space-y-8">
-                    <PostsFilter />
+                    <PostsFilter setFilter={setFilter} />
 
-                    {posts?.map((post: Post) => (
+                    {!posts && !loading && <NoPost />}
+
+                    {sortedPosts.map((post: Post) => (
                         <PostCard key={post.id} post={post} />
                     ))}
 
