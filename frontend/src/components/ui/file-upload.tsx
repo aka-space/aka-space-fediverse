4dd552c16ce142b8d7f28e816931485e96ec70bc
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-11-13 11:43:31 +0700

    style: fix class name tailwindcss

--- frontend/src/components/ui/file-upload.tsx
+++ frontend/src/components/ui/file-upload.tsx
@@ -67,7 +67,7 @@ export const FileUpload = ({
                     }
                     className="hidden"
                 />
-                <div className="absolute inset-0 [mask-image:radial-gradient(ellipse_at_center,white,transparent)]">
+                <div className="absolute inset-0 mask-[radial-gradient(ellipse_at_center,white,transparent)]">
                     <GridPattern />
                 </div>
                 <div className="flex flex-col items-center justify-center">
@@ -102,7 +102,7 @@ export const FileUpload = ({
                                             initial={{ opacity: 0 }}
                                             animate={{ opacity: 1 }}
                                             layout
-                                            className="rounded-lg px-2 py-1 w-fit flex-shrink-0 text-sm text-neutral-600 dark:bg-neutral-800 dark:text-white shadow-input"
+                                            className="rounded-lg px-2 py-1 w-fit shrink-0 text-sm text-neutral-600 dark:bg-neutral-800 dark:text-white shadow-input"
                                         >
                                             {(
                                                 file.size /
@@ -181,14 +181,14 @@ export function GridPattern() {
     const columns = 41;
     const rows = 11;
     return (
-        <div className="flex bg-gray-100 dark:bg-neutral-900 flex-shrink-0 flex-wrap justify-center items-center gap-x-px gap-y-px  scale-105">
+        <div className="flex bg-gray-100 dark:bg-neutral-900 shrink-0 flex-wrap justify-center items-center gap-x-px gap-y-px  scale-105">
             {Array.from({ length: rows }).map((_, row) =>
                 Array.from({ length: columns }).map((_, col) => {
                     const index = row * columns + col;
                     return (
                         <div
                             key={`${col}-${row}`}
-                            className={`w-10 h-10 flex flex-shrink-0 rounded-[2px] ${
+                            className={`w-10 h-10 flex shrink-0 rounded-[2px] ${
                                 index % 2 === 0
                                     ? 'bg-gray-50 dark:bg-neutral-950'
                                     : 'bg-gray-50 dark:bg-neutral-950 shadow-[0px_0px_1px_3px_rgba(255,255,255,1)_inset] dark:shadow-[0px_0px_1px_3px_rgba(0,0,0,1)_inset]'
