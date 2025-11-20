0f0b6ebb3c71d86a35ea171856bf7aa6138742d9
akagiyuu <khanghuynh0245@gmail.com>
2025-11-20 20:40:12 +0700

    feat(api/post): add map from raw post to post

--- backend/src/controller/post/mod.rs
+++ backend/src/controller/post/mod.rs
@@ -1,11 +1,18 @@
 mod create;
 mod update;
 
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
-use axum::{Router, routing};
+use axum::{Router, http::StatusCode, routing};
+use chrono::{DateTime, Utc};
+use sqlx::PgPool;
+use uuid::Uuid;
 
-use crate::state::ApiState;
+use crate::{
+    database::{self, account::MinimalAccount, reaction::Reaction},
+    error::{Error, Result},
+    state::ApiState,
+};
 
 pub use create::*;
 pub use update::*;
@@ -14,4 +21,69 @@ pub fn build() -> Router<Arc<ApiState>> {
     Router::new()
         .route("/post", routing::post(create))
         .route("/post/{id}", routing::put(update))
+}
+
+#[derive(Debug)]
+pub struct Post {
+    pub id: Uuid,
+    pub slug: String,
+
+    pub author: MinimalAccount,
+    pub title: String,
+    pub content: String,
+    pub view: i32,
+    pub reactions: HashMap<Reaction, u64>,
+
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+}
+
+impl Post {
+    pub async fn from_raw(raw: database::post::Post, database: &PgPool) -> Result<Post> {
+        let author = match database::account::get(raw.author_id, database).await {
+            Ok(Some(author)) => author,
+            Ok(None) => {
+                tracing::error!("Post's author is banned");
+
+                return Err(Error::builder()
+                    .status(StatusCode::BAD_REQUEST)
+                    .message("Post's author is banned".to_string())
+                    .build());
+            }
+            Err(error) => {
+                tracing::error!(?error, "Failed to get user");
+
+                return Err(Error::builder()
+                    .status(StatusCode::BAD_REQUEST)
+                    .message("Post's author is banned".to_string())
+                    .build());
+            }
+        };
+        let reactions = match database::reaction::count_by_post(raw.id, database).await {
+            Ok(reactions) => reactions,
+            Err(error) => {
+                tracing::error!(?error, "Failed to count reaction for post");
+
+                return Err(Error::builder()
+                    .status(StatusCode::BAD_REQUEST)
+                    .message("Post is removed".to_string())
+                    .build());
+            }
+        };
+
+        Ok(Post {
+            id: raw.id,
+            slug: raw.slug,
+            author: MinimalAccount {
+                email: author.email,
+                username: author.username,
+            },
+            title: raw.title,
+            content: raw.content,
+            view: raw.view,
+            reactions,
+            created_at: raw.created_at,
+            updated_at: raw.updated_at,
+        })
+    }
 }
