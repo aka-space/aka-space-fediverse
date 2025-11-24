f442fff88743c49fccc8f584524f3a4a43693246
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-11-24 15:45:25 +0700

    refactor: change to use api get posts

--- frontend/src/hooks/post/use-get-posts.ts
+++ frontend/src/hooks/post/use-get-posts.ts
@@ -1,27 +1,40 @@
+import { axiosInstance } from '@/lib/axios';
 import { Post } from '@/types';
 import { useQuery } from '@tanstack/react-query';
-const API_URL = 'https://68765855814c0dfa653bba48.mockapi.io/mockTest';
+import { toast } from 'sonner';
 
-export const useGetPosts = (search: string = '') => {
-    return useQuery<Post[], Error>({
-        queryKey: ['posts', search],
+export const useGetPosts = (search = '', limit = 10, offset = 0) => {
+    return useQuery<{ data: Post[]; hasMore: boolean }, Error>({
+        queryKey: ['posts', search, limit, offset],
         queryFn: async () => {
-            const response = await fetch(API_URL, {
-                method: 'GET',
-            });
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const data: Post[] = await response.json();
+            try {
+                const response = await axiosInstance.get(`/post?limit=${limit}&offset=${offset}`);
 
-            if (search.trim() === '') return data;
-            return data.filter(
-                (post: Post) =>
-                    post.title.toLowerCase().includes(search.toLowerCase()) ||
-                    post.content.toLowerCase().includes(search.toLowerCase()),
-            );
+                if (response.status === 200) {
+                    const data: Post[] = response.data.data;
+                    const hasMore = response.data.has_next;
+
+                    if (search.trim() === '') return {data, hasMore};
+                    return {data: data.filter(
+                        (post: Post) =>
+                            post.title
+                                .toLowerCase()
+                                .includes(search.toLowerCase()) ||
+                            post.content
+                                .toLowerCase()
+                                .includes(search.toLowerCase()),
+                    ), hasMore};
+                }
+
+                return { data: [], hasMore: false };
+            } catch (error) {
+                console.error('Error fetching posts:', error);
+                toast.error('Error fetching posts');
+                throw new Error('Error fetching posts');
+            }
         },
-        staleTime: 5000,
-        placeholderData: [] as Post[],
+        staleTime: 0,
+        gcTime: 5 * 60 * 1000,
+        placeholderData: { data: [], hasMore: false },
     });
 };
