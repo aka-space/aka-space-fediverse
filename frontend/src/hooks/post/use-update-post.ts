438adc542151d0c5652e647381ec2f69be945004
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-11-27 09:53:40 +0700

    feat: add header for api

--- frontend/src/hooks/post/use-update-post.ts
+++ frontend/src/hooks/post/use-update-post.ts
@@ -1,15 +1,22 @@
 'use client';
 
 import { axiosInstance } from '@/lib/axios';
+import { useAuthStore } from '@/store/useAuthStore';
 import { Post } from '@/types';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export const useUpdatePost = () => {
     const queryClient = useQueryClient();
+    const token = useAuthStore((s) => s.accessToken);
 
     return useMutation({
         mutationFn: async (data: Post) => {
-            const response = await axiosInstance.put(`/post/${data.id}`, data);
+            const response = await axiosInstance.put(`/post/${data.id}`, data,{
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+            });
             if (response.status !== 204) {
                 throw new Error('Failed to update post');
             }
