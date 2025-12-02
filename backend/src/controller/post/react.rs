0663e6b1f7d299a6079edb6b3cdce044ee04c5a9
akagiyuu <khanghuynh0245@gmail.com>
2025-12-02 11:37:18 +0700

    feat(api): add post/{id}/react

---
+++ backend/src/controller/post/react.rs
@@ -0,0 +1,54 @@
+use std::sync::Arc;
+
+use axum::{
+    Json,
+    extract::{Path, State},
+    http::StatusCode,
+};
+use axum_extra::{
+    TypedHeader,
+    headers::{Authorization, authorization::Bearer},
+};
+use serde::Deserialize;
+use utoipa::ToSchema;
+use uuid::Uuid;
+use validator::Validate;
+
+use crate::{
+    database::{self, reaction::Reaction},
+    error::{ApiError, ApiResult, ResultExt},
+    state::ApiState,
+};
+
+#[derive(Debug, ToSchema, Deserialize)]
+#[schema(as = post::react::Request)]
+pub struct Request {
+    pub kind: Reaction,
+}
+
+#[utoipa::path(
+    post,
+    tag = "Post",
+    path = "/post/{id}/react",
+    params(
+        ("id" = Uuid, description = "Post id (UUID)", example = json!("3fa85f64-5717-4562-b3fc-2c963f66afa6"))
+    ),
+    request_body = Request,
+    security(("jwt_token" = [])),
+)]
+#[tracing::instrument(err(Debug), skip(state))]
+pub async fn react(
+    State(state): State<Arc<ApiState>>,
+    TypedHeader(bearer): TypedHeader<Authorization<Bearer>>,
+    Path(id): Path<Uuid>,
+    Json(request): Json<Request>,
+) -> ApiResult<StatusCode> {
+    let token = bearer.token();
+    let author_id = state.token_service.access.decode(token)?;
+
+    database::reaction::react(id, author_id, request.kind, &state.database)
+        .await
+        .with_context(StatusCode::BAD_REQUEST, "Invalid post id")?;
+
+    Ok(StatusCode::NO_CONTENT)
+}
