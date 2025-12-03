06c6ad41f3b69771aeb5d28b66bf1eed25ecafe2
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-12-03 16:19:39 +0700

    feat: add hook to create reply

---
+++ frontend/src/hooks/comment/use-create-reply.ts
@@ -0,0 +1,45 @@
+import { axiosInstance } from '@/lib/axios';
+import { useAuthStore } from '@/store/useAuthStore';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'sonner';
+
+interface CreateReplyPayload {
+    content: string;
+}
+
+export const useCreateReply = (commentId: string) => {
+    const queryClient = useQueryClient();
+    const token = useAuthStore((s) => s.accessToken);
+
+    return useMutation({
+        mutationFn: async (data: CreateReplyPayload) => {
+            const response = await axiosInstance.post(
+                `/comment/${commentId}/reply`,
+                data,
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+
+            if (response.status !== 201) {
+                throw new Error('Failed to create reply');
+            }
+
+            return response.data;
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ 
+                queryKey: ['child-comments', commentId],
+                exact: true 
+            });
+            toast.success('Reply posted successfully');
+        },
+        onError: (error) => {
+            console.error('Error creating reply:', error);
+            toast.error('Failed to post reply');
+        },
+    });
+};
